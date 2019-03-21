b533cd7ade9dd88b6cd5ed668b8c38be41a9a866
Paula <paula@trosbach.com>
2019-03-21 11:19:19 +0100

    korrigierte argmin; fertige sumclus (Summiert die Punkte, die zu einem Cluster dazu gehören)

--- kmeans.cpp
+++ kmeans.cpp
@@ -4,9 +4,11 @@
 #include <limits.h>
 #include <cstdlib>
 #include <tgmath.h>
+#include <array>
 using namespace std;
 
 
+//random Float erstellen (funktioniert nicht wirklich???)
 float randomFloat(float min, float max) {
     return (min + 1) + (((float) rand()) / (float) RAND_MAX) * (max - (min + 1)); 
 }
@@ -59,12 +61,12 @@ struct Set* createSet(int amount)
 // berechnet für jeden Punkt den Abstand zu jedem Zentrum -> return: Zentrum mit kleinstem Abstand
 struct Point* argmin(struct Point* p, struct Cluster* clus){
     struct Point* zentrum;
-    int min = INT_MAX;
+    float min = INT_MAX;
     for(int i = 0; i < clus->number; i++){
         float abst = dist(p, &clus->middles[i]);
         if(abst < min){
             min = abst;
-            zentrum = &clus -> middles[i];
+            zentrum = &clus->middles[i];
         }
     }
     return zentrum;
@@ -72,20 +74,61 @@ struct Point* argmin(struct Point* p, struct Cluster* clus){
 
 // Cluster mit Zufallszahlen initiieren
 void initClusters(struct Cluster* clus){
+    clus->middles[0].x = 1.1;
+    clus->middles[0].y = 1.1;
+
+    for(int i = 1; i < clus->number; i++){
+     //   clus->middles[i].x = randomFloat(1,5);
+     //   clus->middles[i].y = randomFloat(1,5);
+    clus->middles[i].x = clus->middles[i-1].x + 0.5;
+    clus->middles[i].y = clus->middles[i-1].x + 0.5;
+    }
+}
+
+void sumclus(struct Point* ergarr[][2], int arrlen, struct Cluster* clus){
     for(int i = 0; i < clus->number; i++){
-        clus->middles[i].x = randomFloat(1,5);
-        clus->middles[i].y = randomFloat(1,5);
+        float sumx = 0;
+        float sumy = 0;
+        for(int j = 0; j < arrlen; j++){
+            if(ergarr[j][1] == &clus->middles[i]){
+                sumx += ergarr[j][0]->x;
+                sumy += ergarr[j][0]->y;
+                // Print zu welchem Kluster ein Punkt gehört
+              //  cout<< "\n"<< j << "\tSumme: " << sumx << "\t" << sumy << "\t" << argmin(ergarr[j][0],clus)->x << "\t" <<  argmin(ergarr[j][0],clus)->y << "\t" << clus->middles[i].x;
+            } 
+        /*   else {
+                cout<< "\n"<< j << "\tfalse ";
+           } */
+        }
+        clus->middles[i].x = sumx;
+        clus->middles[i].y = sumy;
     }
 }
 
 // eigentlicher Algorithmus
 void alg(struct Set* set, struct Cluster* clus)
 {
-    bool change = true;
-    while(change){
+    // Ergebnisarray: zu jedem Pounkt das zugehörige Cluster; initialisierung -> Cluster: null
+    struct Point* ergarr[set->amount][2];
+    for(int i = 0; i < set->amount; i++){
+        ergarr[0][i] = &set->points[i];
+    }
+    bool changec = true;
+
+    // solange sich die Cluster verändern
+    while(changec){
+        changec = false;
+
+        // für jeden Punkt das Cluster mit dem geringsten Abstand berechnen
         for(int i = 0; i < set->amount; i++){
-            argmin(&(set->points[i]), clus);
+            Point* minclus = argmin(&set->points[i], clus);
+            if (minclus != ergarr[1][i]){
+                ergarr[1][i] = minclus;
+                changec = true;
+            }
         }
+
+        //Cluster neu berechnen (Summe über alle Punkte, die zum gleichen Cluster gehören)
     }
 }
 
@@ -96,18 +139,18 @@ main() {
     struct Set* test  = createSet(5);
 
     // Punkte erstellen, an Liste anhängen
-    test->points[0].x = 1.1;
-    test->points[0].y = 1.1;
-    test->points[1].x = 1.3;
-    test->points[1].y = 1.4;
-    test->points[2].x = 2.3;
-    test->points[2].y = 2.6;
-    test->points[3].x = 3.0;
-    test->points[3].y = 2.1;
-    test->points[4].x = 4.6;
-    test->points[4].y = 3.3;
-    test->points[5].x = 3.9;
-    test->points[5].y = 2.4;
+    test->points[0].x = 1;
+    test->points[0].y = 2;
+    test->points[1].x = 2;
+    test->points[1].y = 3;
+    test->points[2].x = 3;
+    test->points[2].y = 4;
+    test->points[3].x = 6;
+    test->points[3].y = 6;
+    test->points[4].x = 9;
+    test->points[4].y = 5;
+    test->points[5].x = 8;
+    test->points[5].y = 8;
 
     // Test Punktewerte
     cout<< "Points:\nx\ty\n";
@@ -124,17 +167,35 @@ main() {
     for(int i = 0; i < number ; i++){
         cout<< clus->middles[i].x << "\t" << clus->middles[i].y << "\n";
     }
-    
+  /*
     // Test dist
     float abst = dist(&test->points[0], &test->points[4]);
     cout<< "\nAbstand zwischen zwei Punkten:\n" << abst << "\n";
-
+*/
     // Test argmin
-    struct Point* punkt = argmin(&test->points[0], clus);
+ /*   struct Point* punkt = argmin(&test->points[0], clus);
     cout<< "\nargmin:\nx:\ty:\n" << punkt->x << "\t" << punkt->y << "\n";
     float d1 = dist(&test->points[0], &clus->middles[0]);
     float d2 = dist(&test->points[0], &clus->middles[1]);
     float d3 = dist(&test->points[0], &clus->middles[2]);
     cout<< "\nAbstand c0:" << d1 << "\tAbstand c1:" << d2 << "\tAbstand c2:" << d3;
+*/
+    // Test sumclus 
+    struct Point* ergarr[test->amount][2];
+    for(int i = 0; i < test->amount; i++){
+        ergarr[i][0] = &test->points[i];
+        ergarr[i][1] = argmin(ergarr[i][0], clus);
+    }
+    int arrlen = sizeof(ergarr)/sizeof(ergarr[0]);
+    sumclus(ergarr, arrlen, clus);
+
+    // Test Clusterwerte
+    cout<< "\nClusters:\nx\ty\n";
+    for(int i = 0; i < number ; i++){
+        cout<< clus->middles[i].x << "\t" << clus->middles[i].y << "\n";
+    }
+
+
+
     
 }
